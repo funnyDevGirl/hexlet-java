52eeecf058307a90e813e4dc6b2e68c767e874e7
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-08 10:00:27 +0300

    Modified One.java and import commons-lang3-3.13.0.jar

--- hexlet-java/io/hexlet/One.java
+++ hexlet-java/io/hexlet/One.java
@@ -1,7 +1,11 @@
 package io.hexlet;
+import org.apache.commons.lang3.StringUtils;
 
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        System.out.println(reverseString("Hexlet"));
+    }
+    public static String reverseString(String str) {
+        return StringUtils.reverse(str);
     }
 }
